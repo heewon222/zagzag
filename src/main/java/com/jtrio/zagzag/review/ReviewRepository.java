763e6689dde0fa80df451d67f4bd0a534341efae
Heewon <khw940114@gmail.com>
2021-01-03 12:56:48 +0900

    0103 과제수정2

--- src/main/java/com/jtrio/zagzag/review/ReviewRepository.java
+++ src/main/java/com/jtrio/zagzag/review/ReviewRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("SELECT avg(totalScore) from Review where product = :prduct")
+    @Query("SELECT avg(totalScore) from Review where product.id=:Id")//id로..
     byte avgProductTotalScore(Long Id);
 }
